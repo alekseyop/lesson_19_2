a93127b45fa57946ed747f7c3e219bb85eeeb578
Aleksey <oszn2011@yandex.ru>
2024-07-10 16:44:31 +0300

    cancel migrate manufactured_at 0001

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'category']
+    list_filter = ['category',]
+    search_fields = ['description',]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name',]
+    search_fields = ['name',]
--- catalog/models.py
+++ catalog/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
-    manufactured_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата производства продукта")
+    # manufactured_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата производства продукта")
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"  # возвращает название товара и название категории
