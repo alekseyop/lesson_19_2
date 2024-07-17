1a916ba7bbf7a837d42a754fb5da94ee0f0d597c
Aleksey <oszn2011@yandex.ru>
2024-07-18 00:56:48 +0300

    templates and controllers created

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    item_name = Product.objects.all()
+    context = {
+        'items': item_name
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -13,3 +19,10 @@ def contacts(request):
         print(name, phone, message)
 
     return render(request, 'catalog/contacts.html')
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    product = Product.objects.get(pk=pk)
+    context = {"item": product}
+    return render(request, 'catalog/product_detail.html', context)
