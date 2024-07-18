51543df65d95ff5ec0e2c522d4f8162fab21e75d
Aleksey <oszn2011@yandex.ru>
2024-07-18 12:39:48 +0300

    added menu template

--- config/settings.py
+++ config/settings.py
@@ -83,12 +83,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+# LANGUAGE_CODE = 'en-us'
+#
+# TIME_ZONE = 'UTC'
+#
+# USE_I18N = True
+#
+# USE_TZ = True
+LANGUAGE_CODE = 'ru-ru'
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
